fa663889868db3244423799e4323ee9d1440f2bd
Neelam Chavan <neelamchavan410@gmail.com>
2022-03-29 15:41:06 +0530

    add expense item card

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+    const expenseDate = new Date();
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 23;
+    return (
+        // <>
+        <div className='expense-item'>
+            <div>{expenseDate.toString()}</div>
+            <div className='expense-item__description'>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>{expenseAmount}</div>
+            </div>
+        </div>
+        // </>
+    )
+}
+
+export default ExpenseItem
