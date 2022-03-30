64dd1b66edfd34117147d13f459af38deed8e82f
Neelam Chavan <neelamchavan410@gmail.com>
2022-03-30 14:50:42 +0530

    add expenses container

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,33 +1,14 @@
 import React from 'react'
 import ExpenseItem from './ExpenseItem';
+import './Expenses.css'
 
-function Expenses() {
-    const expenses = [
-        {
-            id: 'e1',
-            title: 'Toilet Paper',
-            amount: 94.12,
-            date: new Date(2020, 7, 14),
-        },
-        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-        {
-            id: 'e3',
-            title: 'Car Insurance',
-            amount: 294.67,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: 'e4',
-            title: 'New Desk (Wooden)',
-            amount: 450,
-            date: new Date(2021, 5, 12),
-        },
-    ];
+function Expenses(props) {
+
     return (
-        <>
-            <ExpenseItem title={expenses[0].title} date={expenses[0].date} amount={expenses[0].amount} />
-            <ExpenseItem title={expenses[1].title} date={expenses[1].date} amount={expenses[1].amount} />
-        </>
+        <div className='expenses'>
+            <ExpenseItem title={props.items[0].title} date={props.items[0].date} amount={props.items[0].amount} />
+            <ExpenseItem title={props.items[1].title} date={props.items[1].date} amount={props.items[1].amount} />
+        </div>
     )
 }
 
