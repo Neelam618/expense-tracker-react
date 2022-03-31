ffaa9d58e4dfb1dae74ca18bb13242f6529b0e98
Neelam Chavan <neelamchavan410@gmail.com>
2022-03-31 15:23:16 +0530

    get form date on form submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,23 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 function ExpenseForm() {
+    const [title, setTitle] = useState("")
+    const [amount, setAmount] = useState("")
+    const [date, setDate] = useState("")
+    //OR
+    // const [userInput, setUserInput] = useState({
+    //     title: '',
+    //     amount: '',
+    //     date: ''
+    // })
+
+    const titleChangeHandler = (event) => {
+        setTitle(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     title: event.target.value
+        // })
+
+        //OR better approach for above      //for handling multiple states
+        // setUserInput((prevState) => {
+        //     return {
+        //         ...prevState,
+        //         title: event.target.value
+        //     }
+        // })
+    }
+
+    const amountChangeHandler = (event) => {
+        setAmount(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     amount: event.target.value
+        // })
+        // setUserInput((prevState) => {
+        //     return {
+        //         ...prevState,
+        //         amount: event.target.value
+        //     }
+        // })
+    }
+    const dateChangeHandler = (event) => {
+        setDate(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     date: event.target.value
+        // })
+        // setUserInput((prevState) => {
+        //     return {
+        //         ...prevState,
+        //         date: event.target.value
+        //     }
+        // })
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+
+        const expenseData = {
+            title: title,
+            amount: amount,
+            date: new Date(date)
+        }
+        console.log(expenseData);
+        setAmount("")
+        setDate("")
+        setTitle('')
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type="text" />
+                    <input type="text" value={title} onChange={titleChangeHandler} />
                 </div>
 
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min='0.01' step='0.01' />
+                    <input type="number" value={amount} min='0.01' step='0.01' onChange={amountChangeHandler} />
                 </div>
 
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min='2019-01-01' max='2022-12-31' />
+                    <input type="date" value={date} min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className="new-expense__actions">
